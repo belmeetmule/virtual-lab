125eca72d6f185db9b14b0be432a456b9b63575a
Mulugeta Mekonnen <belmeetmule@gmail.com>
2024-06-18 13:34:09 +0300

    Added the nav and hero section to the app component

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Navbar from './components/Navbar'
+import HeroSection from './components/HeroSection'
+
+const App = () => {
+
+  return (
+    <>
+    <Navbar />
+    <HeroSection />
+    </>
+     
+  )
+}
+
+
+export default App
