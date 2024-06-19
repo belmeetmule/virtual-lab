9188fc34ca0ec2aa6d57bbe838196a48b1265d2f
Mulugeta Mekonnen <belmeetmule@gmail.com>
2024-06-19 12:15:33 +0300

    Add components to the app component

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,23 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/Navbar'
 import HeroSection from './components/HeroSection'
+import FeaturesSection from './components/FeaturesSection'
+import Workflow from './components/Workflow'
+import PriceSection from './components/PriceSection'
+import Testimonial from './components/Testimonial'
+import Footer from './components/Footer'
 
 const App = () => {
 
   return (
     <>
-    <Navbar />
-    <HeroSection />
+      <Navbar />
+      <HeroSection />
+      <FeaturesSection />
+      <Workflow />
+      <PriceSection />
+      <Testimonial />
+      <Footer />
     </>
      
   )
